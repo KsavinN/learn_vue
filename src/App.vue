552e0510c20e292d01a6a7a476ada13555937791
ruthos <62721657+ruthos@users.noreply.github.com>
2020-03-26 21:02:44 +0100

    first

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <button-counter></button-counter>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+
+Vue.component("button-counter", {
+  data: function() {
+    return {
+      count: 0
+    };
+  },
+  template:
+    '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+});
+
+export default {
+  name: "App",
+  data: () => ({
+    txt: "test"
+  }),
+  methods: {}
+};
+</script>
+
+<style>
+#app {
+  text-align: left;
+  color: cadetblue;
+}
+h4 {
+  font-size: 16px;
+}
+</style>
